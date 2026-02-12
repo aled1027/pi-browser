18a819e9d4378f3ac177bab9b50b33a58c0c8b94
Alex Ledger <alex@alexledger.net>
2026-02-12 11:49:38 -0800

    Update +page.svelte

--- examples/do-i-suck-at-math/src/routes/+page.svelte
+++ examples/do-i-suck-at-math/src/routes/+page.svelte
@@ -2,8 +2,10 @@
   import { Agent } from "pi-browser";
   import { onMount } from "svelte";
 
+  let loading = $state(true);
   let agent: Agent | null = $state(null);
   let response = $state("");
+  let asking = $state(false);
 
   function saveApiKey(apiKey: string) {
     localStorage.setItem("openrouterApiKey", apiKey);
@@ -17,6 +19,7 @@
     event.preventDefault();
     const formData = new FormData(event.target as HTMLFormElement);
     const apiKey = formData.get("openrouterApiKey") as string;
+    saveApiKey(apiKey);
     initializeAgent(apiKey);
   }
 
@@ -28,16 +31,29 @@
       promptTemplates: [],
       systemPrompt: "respond like a cat, but only in 10 words or less",
     });
-    console.log(agent);
-    const result = await agent.send("Hello, how are you?");
+  }
+
+  async function submitQuestion(event: Event) {
+    if (!agent) return;
+
+    asking = true;
+    event.preventDefault();
+    const formData = new FormData(event.target as HTMLFormElement);
+    const question = formData.get("question") as string;
+
+    const result = await agent.send(question);
     response = result.text;
+    asking = false;
   }
 
   onMount(() => {
+    console.log("onMount");
     const apiKey = getApiKey();
+    console.log("apiKey", apiKey);
     if (apiKey) {
       initializeAgent(apiKey);
     }
+    loading = false;
   });
 </script>
 
@@ -47,17 +63,21 @@
   documentation
 </p>
 
-{#if !agent}
+{#if !agent && !loading}
   <form onsubmit={submitInitializeAgent}>
     <input type="text" name="openrouterApiKey" />
     <button type="submit">Initialize Agent</button>
   </form>
+{:else if agent && !loading}
+  <form onsubmit={submitQuestion}>
+    <input type="text" name="question" />
+    <button type="submit">Ask Question</button>
+  </form>
+  {#if asking}
+    <pre>Asking...</pre>
+  {:else}
+    <pre>{response}</pre>
+  {/if}
+
+  <!-- <pre>{JSON.stringify(agent.tools, null, 2)}</pre> -->
 {/if}
-
-<pre>
-  {response}
-</pre>
-
-<pre>
-  {JSON.stringify(agent, null, 2)}
-</pre>
